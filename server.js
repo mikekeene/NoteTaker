9ef9e432d94a289e9986540115aa65dd25ac46e3
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-10 19:05:11 -0600

    save now working

--- server.js
+++ server.js
@@ -10,10 +10,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// HTML ROUTES: 
+// API ROUTES: 
+//route to get the notes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
+
+//route to add a note
+app.post('/api/notes', (req, res) => {
+    let addNote = req.body;
+    addNote.id = (notes.length + 1) + 1; //gives note unique id
+    notes.push(addNote);
+
+    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes, null, 2), (error) => {
+        if (error) throw error;
+    });
+    res.json(addNote);
+});
+//route to delete a note
+
+// HTML ROUTES: 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
